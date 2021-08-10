db2e4b57c75621e7ba6760ff0519253d6edf22c1
boyoanu <85933732+boyoanu@users.noreply.github.com>
2021-08-10 21:21:56 +0100

    Add files via upload

---
+++ AsyncPractice/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace AsyncPractice
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            AsyncLibrary asyncLibrary = new AsyncLibrary();
+
+            //Task webTask = asyncLibrary.DisplayWebpageContentAsync("https://thebulb.africa/community");
+            Task<int> textCountTask = asyncLibrary.GetCharacterCountAsync();
+            //Task<int> additionTask = asyncLibrary.AddNums1to20Async();
+
+            // asyncLibrary.DisplayWebpageContentAsync("https://thebulb.africa/community").Wait();
+            // Console.WriteLine($"The text file contains {asyncLibrary.GetCharacterCountAsync().Result} characters.");
+            // Console.WriteLine(asyncLibrary.AddNums1to20Async().Result);
+
+           // Task.WaitAll(webTask, additionTask, textCountTask);
+
+            //Console.WriteLine($"The text file contains {textCountTask.Result} characters.");
+            Console.WriteLine(additionTask.Result);
+        }
+    }
+}
